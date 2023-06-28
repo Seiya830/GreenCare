b7419659a6a99797ad05b57be300ae796e4ebd2e
Seiya830 <seiya@MacBook-Pro.local>
2023-06-28 11:13:41 +0900

    create userファイル追加

---
+++ server/api/users.post.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+  const body = await readBody(event)
+  let user = null
+
+  if (body.name)
+    await prisma.users.create({
+      data: {
+        name: body.name,
+      },
+    }).then((response) => {
+      user = response
+    })
+
+  return {
+    user: user
+  }
+})
