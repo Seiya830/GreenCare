f19ebabccec38e863ac3d73429b5b892970c6e03
Seiya830 <seiya@MacBook-Pro.local>
2023-06-28 11:24:12 +0900

    migration.sqlを削除

--- prisma/migrations/20230619025225_initial_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `plants` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `plant_name` VARCHAR(191) NOT NULL,
-    `last_watered_date` DATETIME(3) NOT NULL,
-    `last_memo` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
