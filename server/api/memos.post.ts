69748535d1e8091cd6824340adb174b7584099bf
Seiya830 <seiya@MacBook-Pro.local>
2023-07-10 14:34:03 +0900

    memoのcreate機能実装

--- server/api/memos.post.ts
+++ server/api/memos.post.ts
@@ -1,5 +1,21 @@
-export default defineEventHandler((event) => {
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+  const body = await readBody(event)
+  let memo = null
+
+  if (body.content)
+    await prisma.memos.create({
+      data: {
+        content: body.content,
+      },
+    }).then((response: any) => {
+      memo = response
+    })
+
   return {
-    api: 'create memos'
+    memo: memo
   }
 })
