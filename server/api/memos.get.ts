10bc35c9c59f576a5789b6d46e48fb496d7b8885
Seiya830 <seiya@MacBook-Pro.local>
2023-07-11 15:33:22 +0900

    memoのread機能追加

--- server/api/memos.get.ts
+++ server/api/memos.get.ts
@@ -1,5 +1,6 @@
-export default defineEventHandler((event) => {
-  return {
-    api: 'read memos'
-  }
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async () => { 
+  return await prisma.memos.findMany()
 })
