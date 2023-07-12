c885b388a5ad2eaa5ce99d4e60b44212810bd9b5
Seiya830 <seiya@MacBook-Pro.local>
2023-07-12 11:13:18 +0900

    memoのupdate機能追加

--- server/api/memos.put.ts
+++ server/api/memos.put.ts
@@ -1,5 +1,25 @@
-export default defineEventHandler((event) => {
-  return {
-    api: 'update memos'
-  }
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+  const body = await readBody(event)
+  const id = body.id
+  const content = body.content
+
+  if (!(id && content)) return createError({ statusCode: 400, statusMessage: "Missing id and content"});
+
+  let memo = null
+
+  if (id && content)
+    memo = await prisma.memos.update({
+      where: {
+        id: id,
+      },
+      data: {
+        content: content,
+      },
+    }) 
+
+  return memo
 })
